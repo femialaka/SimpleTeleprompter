d1695fef4baa524a02b4aee68efda53725d3e65a
femialaka <femialakadev@gmail.com>
2022-06-22 21:38:01 +0100

    Initial Commit

---
+++ SimpleTeleprompter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleTeleprompter
+//
+//  Created by femi alaka on 22/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleTeleprompter/SimpleTeleprompterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTeleprompterApp.swift
+//  SimpleTeleprompter
+//
+//  Created by femi alaka on 22/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTeleprompterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
